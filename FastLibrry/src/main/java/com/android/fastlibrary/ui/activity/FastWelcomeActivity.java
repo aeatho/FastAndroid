1c6fd003fb03444192ec8ab830c3f706e94cd2b8
loQua <loquaciouser@gmail.com>
2014-08-19 13:09:14 +0800

    Add FastWelecome Templete

--- FastLibrry/src/main/java/com/android/fastlibrary/ui/activity/FastWelcomeActivity.java
+++ FastLibrry/src/main/java/com/android/fastlibrary/ui/activity/FastWelcomeActivity.java
@@ -1,8 +1,20 @@
 /*
- * Copyright © 2014 上海复高计算机科技有限公司. All rights reserved.
+ * Copyright (C) 2014 loQua.Xee <loquaciouser@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-package com.fugao.common.ui.activity;
+package com.android.fastlibrary.ui.activity;
 
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -13,7 +25,6 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.android.fastlibrary.R;
-import com.android.fastlibrary.ui.activity.BaseActivity;
 
 import java.util.ArrayList;
 import java.util.List;
